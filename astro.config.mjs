4373e90528ebf06b27341238b4f7cfaddeb269d8
Mogami <mogami.creative@gmail.com>
2024-05-01 15:16:31 +0200

    fix: Issue in the astro and tailwind relative files reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  base: './',
+  integrations: [tailwind(), relativeLinks()]
 });
