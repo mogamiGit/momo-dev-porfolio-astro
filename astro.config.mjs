2d2f632086f84b0542879a7ef4071408b7f9da7a
Mogami <mogami.creative@gmail.com>
2024-05-05 14:55:37 +0200

    feat: Added compatibility with cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import relativeLinks from "astro-relative-links";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mogamihub.xyz',
   base: './',
-  integrations: [tailwind(), relativeLinks()]
+  integrations: [tailwind(), relativeLinks()],
+  output: "server",
+  adapter: cloudflare()
 });
