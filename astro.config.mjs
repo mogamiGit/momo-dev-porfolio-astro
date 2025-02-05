52cd5a0a2d72da420dd053f0fad8ba89d5a4dc1d
Mónica Galán <mogami.creative@gmail.com>
2025-02-05 17:07:27 +0100

    update astro and relative links. Delete cloudflare. Change project to output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import relativeLinks from "astro-relative-links";
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mogamihub.xyz',
-  base: './',
+  base: '/',
   integrations: [tailwind(), relativeLinks(), react()],
-  output: "server",
-  adapter: cloudflare()
+  output: "static",
 });
