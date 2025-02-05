e6738f4022229c953227b051f075349e7d46f250
Mónica Galán <mogami.creative@gmail.com>
2025-02-05 17:22:51 +0100

    Revert "update astro and relative links. Delete cloudflare. Change project to output static"
    This reverts commit 52cd5a0a2d72da420dd053f0fad8ba89d5a4dc1d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import relativeLinks from "astro-relative-links";
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mogamihub.xyz',
-  base: '/',
+  base: './',
   integrations: [tailwind(), relativeLinks(), react()],
-  output: "static",
+  output: "server",
+  adapter: cloudflare()
 });
