3ee2fdd6c711f73c4b9c47438a006adebafcbc45
Mónica Galán <mogami.creative@gmail.com>
2025-02-20 13:40:49 +0100

    fix: change astro components for react components
    Button, Check convert to tsx. CardProject added. Delete use client from React components

---
+++ src/components/Check.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface Props {
+    text: string;
+}
+
+const check: React.FC<Props> = ({ text }) => {
+    return (
+        <div>
+            <div className="flex item-center pb-2">
+                <img
+                    className="w-6 h-6"
+                    src="images/check-vector.svg"
+                    alt="chack symbol"
+                />
+                <p className="font-light italic">
+                    {text}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default check;
+
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React from 'react';
 import * as motion from "motion/react-client";
 import classNames from 'classnames';
