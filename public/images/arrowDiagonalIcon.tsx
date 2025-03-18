c215450f309e520417ed2f5eafa7d945d43405a0
Mónica Galán <mogami.creative@gmail.com>
2025-03-18 17:14:45 +0100

    feat: InfoDetail and CardTech component added

--- public/images/arrowDiagonalIcon.tsx
+++ public/images/arrowDiagonalIcon.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 import classNames from 'classnames';
 
 interface Props {
-      className?: string;
+  className?: string;
 }
 
 export const ArrowDiagonalIcon: React.FC<Props> = ({
-      className
+  className
 }) => {
-      const iconClass = classNames(
-            'h-3 stroke-current fill-current',
-            className,
-      );
+  const iconClass = classNames(
+    'h-4 stroke-current fill-current',
+    className,
+  );
 
-      return (
-            <svg
-                  viewBox="0 0 14 14"
-                  fill="none"
-                  className={iconClass}
-                  xmlns="http://www.w3.org/2000/svg"
-            >
-                  <path d="M3 11L11 3" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-                  <path d="M10.9998 8.40263V3H5.59717" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-      );
+  return (
+    <svg
+      viewBox="0 0 14 14"
+      fill="none"
+      className={iconClass}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M3 11L11 3" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M10.9998 8.40263V3H5.59717" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  );
 };
 
 export default ArrowDiagonalIcon;
--- src/components/DetailProject/InfoDetail.tsx
+++ src/components/DetailProject/InfoDetail.tsx
@@ -1,26 +1,39 @@
 import React from "react";
 import SectionDescription from "../SectionDescription";
+import Button from "../Button";
+import ArrowDiagonalIcon from "../../../public/images/arrowDiagonalIcon";
 
-interface Props {
-      year: string;
-      client?: string;
-      description: string;
-      highlights?: string[];
+interface Button {
+  link: string;
+  text: string;
 }
 
-const infoDetail: React.FC<Props> = ({ year, client, description, highlights }) => {
-      return(
-            <div className="w-full grid grid-cols-3 gap-12">
-                  <div className="flex flex-col gap-10 col-span-2">
-                        <SectionDescription title="year" description={year} />
-                        {client && <SectionDescription title="client" description={client} /> }
-                        <SectionDescription title="description" description={description} />
-                  </div>
-                  <div>
-                        <SectionDescription title="highlights" tags={highlights} />
-                  </div>
-            </div>
-      );
-} 
+interface Props {
+  year: string;
+  client?: string;
+  description: string;
+  highlights?: string[];
+  button?: Button;
+}
+
+const infoDetail: React.FC<Props> = ({ year, client, description, highlights, button }) => {
+  return (
+    <div className="w-full grid gap-12 px-8 grid-cols-1 md:grid-cols-3 pb-10 md:pb-4">
+      <div className="flex flex-col gap-10 col-span-2">
+        <SectionDescription title="year" description={year} />
+        {client && <SectionDescription title="client" description={client} />}
+        <SectionDescription title="description" description={description} />
+      </div>
+      <div className="flex flex-col gap-16 md:gap-20">
+        <SectionDescription title="highlights" tags={highlights} />
+        {button &&
+          <a href={button.link} target="_blank">
+            <button className="group rounded border-2 dark:border dark:border-white border-dark-blue px-4 py-2 hover:border-blue-violet dark:hover:border-blue-violet transition-all ease-in-out flex gap-2 items-center">{button.text} <ArrowDiagonalIcon className="group-hover:text-blue-violet" /></button>
+          </a>
+        }
+      </div>
+    </div>
+  );
+}
 
 export default infoDetail;
