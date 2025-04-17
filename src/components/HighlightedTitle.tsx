02f5e87b20d36f2dac4d54b0e7d344c1c519fa96
Mónica Galán <mogami.creative@gmail.com>
2025-04-17 23:15:17 +0200

    fix: HighlightedTitle animation simplified, Tag animation fixed

--- src/components/HighlightedTitle.tsx
+++ src/components/HighlightedTitle.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import * as motion from "motion/react-client";
 
 interface Props {
@@ -7,26 +7,21 @@ interface Props {
 }
 
 const HighlightedTitle: React.FC<Props> = ({ title, subtitle }) => {
-  const [hasAnimated, setHasAnimated] = useState(false);
-
   const containerClass = "flex flex-col justify-center items-center gap-2 lg:py-0 md:py-5";
   const titleClass = "text-3xl text-yellow-chick";
-
-  useEffect(() => {
-    setHasAnimated(true);
-  }, []);
 
   return (
     <div className={containerClass}>
       <motion.p
         className={titleClass}
         initial={{ scale: 1.1 }}
-        animate={hasAnimated ? { scale: 0.9 } : { scale: 1.1 }} 
+        whileInView={{ scale: 0.9 }}
         transition={{ duration: 0.5, type: "spring", bounce: 0.2 }}
+        viewport={{ once: true }}
       >
         {title}
       </motion.p>
-      <p className='text-sm text-gray-500'>{subtitle}</p>
+      <p className="text-sm text-gray-500">{subtitle}</p>
     </div>
   );
 };
