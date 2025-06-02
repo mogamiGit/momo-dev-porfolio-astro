5941ebef07f75b7f88c4c9fee9615d6c9a136117
Mónica Galán <mogami.creative@gmail.com>
2025-06-02 21:31:42 +0200

    feat: changed cloudflare pages to vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,13 @@ import relativeLinks from "astro-relative-links";
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mogamihub.xyz',
   base: './',
   integrations: [tailwind(), relativeLinks(), react()],
   output: "server",
-  adapter: cloudflare({
-    mode: 'directory',
-    functionPerRoute: false
-  })
+  adapter: vercel()
 });
